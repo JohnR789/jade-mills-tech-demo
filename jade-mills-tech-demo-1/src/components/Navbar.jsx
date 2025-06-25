2b2637a69e91908e6095b28661827f2a53605f22
John <rollinsj789@gmail.com>
2025-06-25 13:48:51 -0400

    1

--- jade-mills-tech-demo-1/src/components/Navbar.jsx
+++ jade-mills-tech-demo-1/src/components/Navbar.jsx
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import MenuOverlay from "./MenuOverlay";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // Optionally close menu on route change
+  React.useEffect(() => {
+    setMenuOpen(false);
+  }, [location]);
 
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src="/logo.png" alt="Logo" /> Autumn Realty
-      </div>
-      <ul className={`nav-links ${open ? "active" : ""}`}>
-        <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
-        <li><Link to="/about" onClick={() => setOpen(false)}>About</Link></li>
-        <li><Link to="/listings" onClick={() => setOpen(false)}>Listings</Link></li>
-        <li><Link to="/press" onClick={() => setOpen(false)}>Press</Link></li>
-        <li><Link to="/contact" onClick={() => setOpen(false)}>Contact</Link></li>
-      </ul>
-      <div className="hamburger" onClick={() => setOpen((o) => !o)}>
-        <span />
-        <span />
-        <span />
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <img src="/logo.png" alt="Logo" /> Autumn Realty
+        </div>
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li className="hide-on-mobile"><Link to="/about">About</Link></li>
+          <li className="hide-on-mobile"><Link to="/listings">Listings</Link></li>
+          <li className="hide-on-mobile"><Link to="/press">Press</Link></li>
+          <li className="hide-on-mobile"><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className="hamburger" onClick={() => setMenuOpen(true)}>
+          <span />
+          <span />
+          <span />
+        </div>
+      </nav>
+      <MenuOverlay open={menuOpen} onClose={() => setMenuOpen(false)} />
+    </>
   );
 };
 
 export default Navbar;
+
 
